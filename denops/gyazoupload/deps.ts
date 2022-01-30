690136ee3159bf3b461547ec6564f76b1c272b0b
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2022-01-30 22:31:33 +0900

    Follow to denops@3.0

--- denops/gyazoupload/deps.ts
+++ denops/gyazoupload/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v2.2.0/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v2.2.0/function/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.0.0/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.0.0/function/mod.ts";
 export * as clip from "https://deno.land/x/clipboard_image@v0.0.1/mod.ts";
 export { ensureString } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
-export { readAll } from "https://deno.land/std@0.115.1/streams/conversion.ts";
+export { readAll } from "https://deno.land/std@0.123.0/streams/conversion.ts";
