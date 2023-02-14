74d787b6de802f9b0857d1dab64cc56d2c169943
gamoutatsumi <47162587+gamoutatsumi@users.noreply.github.com>
2023-02-14 17:55:35 +0900

    update dependencies

--- denops/gyazoupload/deps.ts
+++ denops/gyazoupload/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.8.2/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.8.2/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.8.2/function/mod.ts";
-export { assertString } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
-export { readAll } from "https://deno.land/std@0.157.0/streams/conversion.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v4.0.0/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v4.0.0/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
+export { assertString } from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
+export { readAll } from "https://deno.land/std@0.177.0/streams/mod.ts";
 export * as clippy from "https://deno.land/x/clippy@v0.2.0/mod.ts";
